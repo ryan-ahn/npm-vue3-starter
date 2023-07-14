7b191ba60a88768f92c23ff07b480717847e1b50
ryan-ahn <kaaiinn4@gmail.com>
2023-07-15 08:47:21 +0900

    Modify: Data Store 샘플 추가

--- src/store/list.ts
+++ src/store/list.ts
@@ -4,5 +4,9 @@ import { defineStore } from 'pinia';
 export const useDataStore = defineStore('data', () => {
   // State
   const data = ref<string>('');
+  // Set State
+  const setDate = (payload: string) => {
+    data.value = payload
+  }
   return { data };
 });
