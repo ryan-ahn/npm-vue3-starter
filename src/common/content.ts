841d2e581d1b2f030c2878660f39b09444e43643
ryan-ahn <kaaiinn4@gmail.com>
2023-08-02 18:41:27 +0900

    Modify: 샘플 페이지 변경

---
+++ src/common/content.ts
@@ -0,0 +1,6 @@
+export const CONTENT_PAGE = {
+  main: {
+    title: 'VUE3\nSTART',
+    description: 'Vue3 Setup을 사용해 보세요.',
+  },
+};
--- src/containers/main/index.vue
+++ src/containers/main/index.vue
@@ -5,18 +5,35 @@
 -->
 
 <template>
-  <section class="wrapper">
-    <h1>샘플페이지</h1>
+  <section class="page-wrapper">
+    <div class="title-box">
+      <h1>{{ contentTitle }}</h1>
+      <p>{{ contentDescription }}</p>
+    </div>
   </section>
 </template>
 
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import { CONTENT_PAGE } from '@common/content';
+// variable
+const contentTitle = CONTENT_PAGE.main.title;
+const contentDescription = CONTENT_PAGE.main.description;
 </script>
 
-<style lang='scss'>
-.wrapper {
-  position: relative;
+<style lang="scss">
+.page-wrapper {
+  @include flexSet(center, center, column);
+  @include boxSet(100%, 100vh, 0px);
+  @include colorSet(white, #111111);
 }
-
+.title-box {
+  @include flexSet(center, center, column);
+  gap: 10px;
+  padding: 50px;
+  & > h1 {
+    @include fontSet(100px, 700, 110px);
+    text-align: center;
+    white-space: pre-wrap;
+  }
+}
 </style>
